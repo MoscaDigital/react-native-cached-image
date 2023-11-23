e3fd0d7eead38e6738ecb3abc7625839e07cf1a2
Francisco Vieira <francisco@digitalspiders.io>
2023-11-23 07:10:58 +0000

    Remove dependency on react-native-clcasher by moving code to this lib

--- __tests__/ImageCacheManager-test.js
+++ __tests__/ImageCacheManager-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-jest.mock('rn-fetch-blob', () => ({default: {fs: {}}}));
-jest.mock('react-native-clcasher/MemoryCache', () => ({default: {}}));
+jest.mock('rn-fetch-blob', () => ({ default: { fs: {} } }));
+jest.mock('../MemoryCache', () => ({ default: {} }));
 
 import ImageCacheManager from '../ImageCacheManager';
 import SimpleMemoryCache from './SimpleMemoryCache';
@@ -10,39 +10,31 @@ import SimpleMemoryFs from './SimpleMemoryFs';
 const icm = ImageCacheManager({}, SimpleMemoryCache, SimpleMemoryFs);
 
 describe('ImageCacheManager', () => {
+  beforeEach(() => icm.clearCache());
 
-    beforeEach(() => icm.clearCache());
-
-    describe('downloadAndCacheUrl', () => {
-
-        it('should fail if URL is not cacheable', () => {
-            return icm.getCacheInfo()
-                .then(res => console.log(res))
-                .then(() => {
-                    return expect(icm.downloadAndCacheUrl('not a real url')).rejects.toBeDefined();
-                });
+  describe('downloadAndCacheUrl', () => {
+    it('should fail if URL is not cacheable', () => {
+      return icm
+        .getCacheInfo()
+        .then((res) => console.log(res))
+        .then(() => {
+          return expect(icm.downloadAndCacheUrl('not a real url')).rejects.toBeDefined();
         });
-
-        it('should download a file when not in cache', () => {
-            return icm.getCacheInfo()
-                .then(res => console.log(res))
-                .then(() => icm.downloadAndCacheUrl('https://example.com/image.jpg'))
-                .then(() => icm.getCacheInfo())
-                .then(res => console.log(res))
-        });
-
-        it('should add new entry to the cache if not in cache', () => {
-
-        });
-
-        it('should return file name if image is in cache', () => {
-
-        });
-
-        it('should not return cached entry if expired', () => {
-
-        });
-
     });
 
+    it('should download a file when not in cache', () => {
+      return icm
+        .getCacheInfo()
+        .then((res) => console.log(res))
+        .then(() => icm.downloadAndCacheUrl('https://example.com/image.jpg'))
+        .then(() => icm.getCacheInfo())
+        .then((res) => console.log(res));
+    });
+
+    it('should add new entry to the cache if not in cache', () => {});
+
+    it('should return file name if image is in cache', () => {});
+
+    it('should not return cached entry if expired', () => {});
+  });
 });
